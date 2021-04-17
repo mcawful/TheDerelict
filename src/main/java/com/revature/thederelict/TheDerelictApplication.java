5be9a30b909ea515def4703a4a38cb2e853a5700
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-04-17 14:31:53 -0400

    Added MainGameLoop

--- src/main/java/com/revature/thederelict/TheDerelictApplication.java
+++ src/main/java/com/revature/thederelict/TheDerelictApplication.java
@@ -1,13 +1,14 @@
 package com.revature.thederelict;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.revature.thederelict.services.MainGameLoop;
 
 @SpringBootApplication
 public class TheDerelictApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TheDerelictApplication.class, args);
+		MainGameLoop.startGameLoop();
 	}
 
 }
