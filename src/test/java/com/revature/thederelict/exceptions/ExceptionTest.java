166d7f029e48fa7e74bd0de3eebde7a9e5322c3a
Carlos <guerreroc47@gmail.com>
2021-04-19 11:39:49 -0700

    update
    added few classes from exception file.

--- src/test/java/com/revature/thederelict/exceptions/ExceptionTest.java
+++ src/test/java/com/revature/thederelict/exceptions/ExceptionTest.java
@@ -1,6 +1,5 @@
 package com.revature.thederelict.exceptions;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
@@ -11,11 +10,30 @@ import org.junit.Test;
 public class ExceptionTest {
 	
 	private IncompatibleActionException impcompatibleActionException;
+	private InvalidExitException invalidExitException;
+	private ItemNotFoundException itemNotFoundException;
+	private UnknownActionException unknownActionException;
 	
 	@Before
 	public void setUp(IncompatibleActionException impcompatibleActionException) throws Exception{
 		this.impcompatibleActionException = impcompatibleActionException;
 	}
+	
+	@Before
+	public void setUp(InvalidExitException invalidExitException) throws Exception{
+		this.invalidExitException = invalidExitException;
+	}
+	
+	@Before
+	public void setUp(ItemNotFoundException itemNotFoundException) throws Exception{
+		this.itemNotFoundException = itemNotFoundException;
+	}
+	
+	@Before
+	public void setUp(UnknownActionException unknownActionException) throws Exception{
+		this.unknownActionException = unknownActionException;
+	}
+	
 	
 	@After
 	public void tearDown() throws Exception{
@@ -24,12 +42,12 @@ public class ExceptionTest {
 	
 	@Test
 	public void whenExceptionThrown_thenAssertionSucceeds() {
-		Exception exception = assertThrows(NumberFormatException.class, () ->{
+		NumberFormatException impcompatibleActionException = assertThrows(NumberFormatException.class, () ->{
 			Integer.parseInt("839@asdf");
 		});
 		
 		String exceptedMessage = "For the action";
-		String actualMessage = exception.getMessage();
+		String actualMessage = impcompatibleActionException.getMessage();
 		
 		assertTrue(actualMessage.contains(exceptedMessage));
 	}
